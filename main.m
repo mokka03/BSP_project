d170f6a58d2d759684adcdcd1a7ff21c8112b138
mokka03 <57620564+mokka03@users.noreply.github.com>
2022-11-11 11:59:31 +0100

    Short time Fourier transform

--- main.m
+++ main.m
@@ -19,15 +19,36 @@ P1 (2: end -1) = 2* P1 (2: end -1) ; % Compute the single - sided spectrum
 
 f = fs *(0:( L /2) )/L ; % Define the frequency
 
+%% STFT
+% Short-time Fourier transform
+[s,f2,t2] = stft(sig,fs,'Window',hann(1024));
+sdb = mag2db(abs(s)); % Amplitude spectrum to dB
+
 %% Plot
 figure(1)
+% Plot signal
 subplot(2,1,1)
 plot(t,sig);
 xlim([t(1) t(end)]);
 xlabel('Time [s]');
 ylabel('Amplitude [a.u.]');
 
+% Plot FFT
 subplot(2,1,2)
 plot(f,P1/max(P1));
 xlabel('Frequency [Hz]');
 ylabel('|A(f)|_{norm} [a. u.]');
+
+% Plot the Short-time Fourier spectrum of the filtered signal
+figure(2)
+mesh(t2/60,f2,sdb);
+cc = max(sdb(:))+[-60 0];
+ax = gca;
+ax.CLim = cc;
+view(2)
+c = colorbar;
+c.Label.String = 'Amplitude [dB]';
+% xlim([0 60]);
+% ylim([-1 1]);
+xlabel('Time [min]');
+ylabel('Frequency [Hz]');
