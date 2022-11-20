ff72429d7eb22ee38c2cdc325e93a54cf4e3c2ca
mokka03 <57620564+mokka03@users.noreply.github.com>
2022-11-20 16:45:25 +0100

    murmur

--- main.m
+++ main.m
@@ -2,15 +2,15 @@
 clear all;
 
 %% Load data
-% fileName = '69141_MV';
-fileName = '85033_TV';
-sig = audioread(strcat("data/normal/", fileName, ".wav"))';
-tlabels = readtable(strcat("data/normal/", fileName, ".tsv"),"FileType","text","Delimiter","tab");
+% % fileName = '69141_MV';
+% fileName = '85033_TV';
+% sig = audioread(strcat("data/normal/", fileName, ".wav"))';
+% tlabels = readtable(strcat("data/normal/", fileName, ".tsv"),"FileType","text","Delimiter","tab");
 
-% % fileName = '9979_TV';
-% fileName = '46579_TV';
-% sig = audioread(strcat("data/murmur/", fileName, ".wav"))';
-% tlabels = readtable(strcat("data/murmur/", fileName, ".tsv"),"FileType","text","Delimiter","tab");
+% fileName = '9979_TV';
+fileName = '49748_TV';
+sig = audioread(strcat("data/murmur/", fileName, ".wav"))';
+tlabels = readtable(strcat("data/murmur/", fileName, ".tsv"),"FileType","text","Delimiter","tab");
 
 %% fs and t
 fs = 4000; % Hz
@@ -38,7 +38,7 @@ fsig = filter(b_high,a_high,fsig);
 
 %% STFT
 % Short-time Fourier transform
-[s,f2_stft,t2] = stft(fsig,fs,'Window',hann(1024));
+[s,f2_stft,t2] = stft(fsig,fs,'Window',hann(512));
 sdb = mag2db(abs(s)); % Amplitude spectrum to dB
 
 %% Envelope using Hilbert transformation
@@ -52,9 +52,9 @@ envelope = envelope/max(envelope);
 [pks, locs] = findpeaks(envelope,'MinPeakDistance',0.17*fs,'MinPeakHeight',mean(envelope)*1); % 0.2s
 
 %% Get heart rate
-HeartRate = getHeartRate(envelope,locs,fs)
+HeartRate = getHeartRate(envelope,locs,fs);
 
-%% Systolic/Diastolic regions
+%% Binary systolic/Diastolic regions
 SD = ones(1,length(fsig));
 
 for loc = locs
@@ -63,42 +63,45 @@ for loc = locs
     end
 end
 
+%% Murmur detection
+pathology = murmur(sig,fs,SD);
+
 %% Plot
-figure(1)
-% Plot filtered signal
-subplot(2,1,1)
-plot(t,fsig/max(fsig));
-hold on;
-plot(t,envelope*5);
-plot(t,5*labelArray/4*max(envelope));
-plot(locs/fs,pks*5,'.');
-plot(t,SD);
-hold off;
-% xlim([0 10]);
-xlim([t(1) t(end)]);
-xlabel('Time [s]');
-ylabel('Amplitude [a.u.]');
-legend('Signal','Envelope [a.u.]','Hert cycle','S1 and S2', 'S/D regions');
-
-% Plot filtered FFT
-subplot(2,1,2)
-plot(f_filtered,fsig_FFT);
-xlim([0 100]);
-xlabel('Frequency [Hz]');
-ylabel('|A(f)|_{norm} [a. u.]');
-
-
-% Plot the Short-time Fourier spectrum of the filtered signal
-figure(2)
-p = pcolor(t2,f2_stft,sdb);
-set(p, 'EdgeColor', 'none');    % Turn off gtid
-cc = max(sdb(:))+[-60 0];
-ax = gca;
-ax.CLim = cc;
-view(2)
-c = colorbar;
-c.Label.String = 'Amplitude [dB]';
-% xlim([0 60]);
-ylim([-100 100]);
-xlabel('Time [min]');
-ylabel('Frequency [Hz]');
+% figure(1)
+% % Plot filtered signal
+% subplot(2,1,1)
+% plot(t,fsig/max(fsig));
+% hold on;
+% plot(t,envelope*5);
+% plot(t,5*labelArray/4*max(envelope));
+% plot(locs/fs,pks*5,'.');
+% plot(t,SD);
+% hold off;
+% % xlim([0 10]);
+% xlim([t(1) t(end)]);
+% xlabel('Time [s]');
+% ylabel('Amplitude [a.u.]');
+% legend('Signal','Envelope [a.u.]','Hert cycle','S1 and S2', 'S/D regions');
+% 
+% % Plot filtered FFT
+% subplot(2,1,2)
+% plot(f_filtered,fsig_FFT);
+% xlim([0 100]);
+% xlabel('Frequency [Hz]');
+% ylabel('|A(f)|_{norm} [a. u.]');
+% 
+% 
+% % Plot the Short-time Fourier spectrum of the filtered signal
+% figure(2)
+% p = pcolor(t2,f2_stft,sdb);
+% set(p, 'EdgeColor', 'none');    % Turn off gtid
+% cc = max(sdb(:))+[-60 0];
+% ax = gca;
+% ax.CLim = cc;
+% view(2)
+% c = colorbar;
+% c.Label.String = 'Amplitude [dB]';
+% % xlim([0 60]);
+% ylim([-100 100]);
+% xlabel('Time [s]');
+% ylabel('Frequency [Hz]');
--- runner.m
+++ runner.m
@@ -6,20 +6,20 @@ labels = readtable("data/normal/85033_TV.tsv","FileType","text","Delimiter","tab
 
 props = project_run(sig);
 %% Folder
-folder = 'data/normal/';
-[props_n,labels_n] = run_for_folder(folder);
+% folder = 'data/normal/';
+% [props_n,labels_n] = run_for_folder(folder);
 folder = 'data/murmur/';
 [props_m,labels_m] = run_for_folder(folder);
 
-props = [props_n,props_m];
-labels = [labels_n;labels_m];
+% props = [props_n,props_m];
+% labels = [labels_n;labels_m];
 %%
-[hit_percent,miss_percent,multihit_percent,hrdiff_percent,ibsegdiff_percent,Se,Sp] = ...
-    calc_score(props,props,labels);
-
-avg_percent = mean([hit_percent;miss_percent;multihit_percent;hrdiff_percent;ibsegdiff_percent]);
-avg_hit_percent = avg_percent(1);
-avg_miss_percent = avg_percent(2);
-avg_multihit_percent = avg_percent(3);
-avg_hrdiff_percent = avg_percent(4);
-avg_ibsegdiff_percent = avg_percent(5);
+% [hit_percent,miss_percent,multihit_percent,hrdiff_percent,ibsegdiff_percent,Se,Sp] = ...
+%     calc_score(props,props,labels);
+% 
+% avg_percent = mean([hit_percent;miss_percent;multihit_percent;hrdiff_percent;ibsegdiff_percent]);
+% avg_hit_percent = avg_percent(1);
+% avg_miss_percent = avg_percent(2);
+% avg_multihit_percent = avg_percent(3);
+% avg_hrdiff_percent = avg_percent(4);
+% avg_ibsegdiff_percent = avg_percent(5);
